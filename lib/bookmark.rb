93afe28500ef4db16bc84ccd81ec0bbf024a1ccf
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-15 16:42:53 +0000

    Test Message

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,7 +4,9 @@ class Bookmark
   def self.all
    
     connection = PG.connect(dbname: 'bookmark_manager')
-    result = connection.exec("SELECT * FROM bookmarks;")
+    result = connection.exec("SELECT * FROM bookmarks")
     result.map { |bookmark| bookmark['url'] }
   end
 end
+
+Hi Trushna
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -2,7 +2,7 @@ feature 'bookmark route' do
   scenario 'it makes sure that bookmark route works' do
     visit '/bookmarks'
 
-    # expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://makers.tech"
     expect(page).to have_content "http://www.destroyallsoftware.com"
     expect(page).to have_content "http://www.google.com"
   end
