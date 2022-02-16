c8e0ae85bce77a67cd64c983f4df462e6738667a
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-16 17:47:36 +0000

    Getting through step 11

--- spec/features/submit_bookmark_spec.rb
+++ spec/features/submit_bookmark_spec.rb
@@ -2,8 +2,11 @@ feature 'submit bookmarks' do
   scenario 'it allows a user to submit bookmarks from the browswer' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://example.org')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
-    expect(page).to have_content 'http://example.org'
+    expect(page).to have_link('Test Bookmark', href: 'http://www.example.org')
 
   end
 end
+
+
